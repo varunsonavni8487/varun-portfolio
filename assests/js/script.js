5f2e62b010c2536adc47fe065f0c9188126c0eaf
varun8487 <varunsonavne@gmail.com>
2021-07-31 20:52:00 +0530

    Chatbot added

--- assests/js/script.js
+++ assests/js/script.js
@@ -102,13 +102,12 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
   (function(){
   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
   s1.async=true;
-  s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
+  s1.src='https://embed.tawk.to/610568d7d6e7610a49adeb0f/1fbui32rt';
   s1.charset='UTF-8';
   s1.setAttribute('crossorigin','*');
   s0.parentNode.insertBefore(s1,s0);
   })();
 // End of Tawk.to Live Chat
-
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
